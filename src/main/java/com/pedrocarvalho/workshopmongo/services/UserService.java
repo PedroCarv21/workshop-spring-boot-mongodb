1180f50214d9e954b1ecb1508176cfa3fd77f2b6
Pedro Lima <pedro8.carvalho@gmail.com>
2025-03-13 11:39:53 -0300

    Inserção de usuário com POST

--- src/main/java/com/pedrocarvalho/workshopmongo/services/UserService.java
+++ src/main/java/com/pedrocarvalho/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedrocarvalho.workshopmongo.domain.User;
+import com.pedrocarvalho.workshopmongo.dto.UserDTO;
 import com.pedrocarvalho.workshopmongo.repository.UserRepository;
 import com.pedrocarvalho.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
