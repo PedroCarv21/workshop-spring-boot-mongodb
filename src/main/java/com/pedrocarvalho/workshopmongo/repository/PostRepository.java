a148cfbdd20414d79f0a13828ef443c84ed619f8
Pedro Lima <pedro8.carvalho@gmail.com>
2025-03-14 13:02:33 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/pedrocarvalho/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.pedrocarvalho.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pedrocarvalho.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
